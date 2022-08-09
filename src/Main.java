b17819c07043bf8989010b98f35c42475016c2e8
PedroEsteves22 <pesteves458@gmail.com>
2022-08-09 01:03:40 -0300

    acrescentando Dev e Bootcamp

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+
 import br.com.dio.desafio.dominio.Curso;
 import br.com.dio.desafio.dominio.Mentoria;
 
@@ -16,13 +17,13 @@ public class Main {
         curso2.setDescricao("descrição curso js");
         curso2.setCargaHoraria(4);
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("mentoria de java");
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
         System.out.println(mentoria);
 
     }
